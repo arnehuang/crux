021fa56eb708f0cb20a3550d36373a5559f91b8b
Arne Huang <a25huang@uwaterloo.ca>
2018-04-13 01:00:10 +0100

    'refactoredMarkdown'

--- Crux.py
+++ Crux.py
@@ -1,9 +1,9 @@
 
 # coding: utf-8
 
-# **Crux Python Assesment**
+# # Crux Python Assesment
 
-# In[1]:
+# In[2]:
 
 
 import requests
@@ -11,7 +11,7 @@ import pandas as pd
 from pathlib import Path
 
 
-# In[2]:
+# In[3]:
 
 
 data = Path("data/data.csv")
@@ -31,19 +31,14 @@ if not mappings.is_file():
         f.write(response.content)
 
 
-# In[3]:
+# In[5]:
 
 
 dfData = pd.read_csv(data, sep=",")
+dfData.head(2)
 
 
-# In[4]:
-
-
-dfData.head(5)
-
-
-# *Investigating Insight Score*
+# # Investigating Insight Score
 
 # In[5]:
 
@@ -51,17 +46,10 @@ dfData.head(5)
 any(dfData.Insight_Score.isnull())
 
 
-# In[68]:
-
-
-dfData.groupby(['Period']).count().head(5)
-
-
 # In[7]:
 
 
-get_ipython().run_line_magic('matplotlib', 'inline')
-dfData.hist(column='Insight_Score', bins=50)
+dfData.groupby(['Period']).count().head(1)
 
 
 # In[8]:
@@ -73,16 +61,23 @@ dfData.groupby(['Industry']).mean()
 # In[9]:
 
 
+get_ipython().run_line_magic('matplotlib', 'inline')
+dfData.hist(column='Insight_Score', bins=50)
+
+
+# In[10]:
+
+
 dfData.plot(x='Period', y='Insight_Score')
 
 
-# Not really sure what Insight_Score is supposed to represent. A credit/performance rating of some kind? 
+# Being given no business context, cannot conclude what Insight_Score is supposed to represent. A credit/performance rating of some kind? As for typing, Insight_Score is a float with high variance. No NA's.
 # 
-# Insight_Score is a float with high variance. No NA's.
 # 
-# Filter by removing outliers?
 
-# In[10]:
+# Filter Insight_Score by removing outliers
+
+# In[12]:
 
 
 sd = dfData.Insight_Score.std()
@@ -90,35 +85,37 @@ mean = dfData.Insight_Score.mean()
 sd, mean
 
 
-# In[11]:
+# In[13]:
 
 
 dfData_filtered = dfData[(dfData.Insight_Score < mean+ 3*sd) & (dfData.Insight_Score > mean-3*sd)]
 
 
-# In[12]:
+# In[14]:
 
 
 dfData_filtered.plot(x='Period', y='Insight_Score')
 
 
-# *Looking into Mappings*
+# The resulting dataframe does not have such drastic spikes as before
 
-# In[13]:
+# # Looking into Mappings
+
+# In[15]:
 
 
 dfMap = pd.read_csv(mappings, sep=",")
 
 
-# In[14]:
+# In[16]:
 
 
 dfMap.head(5)
 
 
-# Inner join (would need more business context to understand if left join makes more sense, etc)
+# Below I inner join to map the columns. More business context may guide decisions such as if left joining makes sense, etc
 
-# In[15]:
+# In[17]:
 
 
 dfMap.rename(columns={'ProductCode': 'Product_Code'}, inplace=True)
@@ -126,17 +123,17 @@ dfMap.rename(columns={'ProductCode': 'Product_Code'}, inplace=True)
 dfmerged = pd.merge(dfData_filtered, dfMap, on='Product_Code')
 
 
-# In[16]:
+# In[18]:
 
 
 dfmerged.head(5)
 
 
-# *Analysis*
+# # Analysis
 
-# Simple outlier detection might be using standard deviation, so anything above or below 3 standard deviations could be flagged, aggregated by Industry, Country, Period, etc depending on the business context. For example,
+# Simple outlier detection might involve using static standard deviation. For Example, any Price above or below 3 standard deviations could be flagged (aggregated by Industry), as shown by the list of outliers below
 
-# In[17]:
+# In[22]:
 
 
 mat_prices = dfmerged[dfmerged.Industry == 'MATERIALS'].Price
@@ -144,154 +141,21 @@ mat_sd = mat_prices.std()
 mat_avg = mat_prices.mean()
 outliers_materials_prices = dfmerged[(dfmerged.Industry == 'MATERIALS') & 
                     ((dfmerged.Price > mat_avg + 3*mat_sd) | (dfmerged.Price < mat_avg - 3*mat_sd))]
-
-
-# In[53]:
-
-
 outliers_materials_prices.head(5)
 
 
-# Since period is a timeseries, could individually analyze by product codes and use, for example, an ARIMA model to detect outliers. To save time, I'll steal some outlier code from https://www.datascience.com/blog/python-anomaly-detection, refactored for python3
+# However, since period is a timeseries, it would make more sense to individually analyze by product codes and use, for example, an ARIMA model to detect outliers. To save time, I'll steal some outlier code from https://www.datascience.com/blog/python-anomaly-detection, refactored for python3
 
-# In[42]:
-
-
-from itertools import count
-import matplotlib.pyplot as plt
-from numpy import linspace, loadtxt, ones, convolve
-import numpy as np
-import pandas as pd
-import collections
-from random import randint
-from matplotlib import style
-style.use('fivethirtyeight')
-
-def moving_average(data, window_size):
-    """ Computes moving average using discrete linear convolution of two one dimensional sequences.
-    Args:
-    -----
-            data (pandas.Series): independent variable
-            window_size (int): rolling window size
-
-    Returns:
-    --------
-            ndarray of linear convolution
-
-    References:
-    ------------
-    [1] Wikipedia, "Convolution", http://en.wikipedia.org/wiki/Convolution.
-    [2] API Reference: https://docs.scipy.org/doc/numpy/reference/generated/numpy.convolve.html
-
-    """
-    window = np.ones(int(window_size))/float(window_size)
-    return np.convolve(data, window, 'same')
+# In[26]:
 
 
-def explain_anomalies(y, window_size, sigma=1.0):
-    """ Helps in exploring the anamolies using stationary standard deviation
-    Args:
-    -----
-        y (pandas.Series): independent variable
-        window_size (int): rolling window size
-        sigma (int): value for standard deviation
-
-    Returns:
-    --------
-        a dict (dict of 'standard_deviation': int, 'anomalies_dict': (index: value))
-        containing information about the points indentified as anomalies
-
-    """
-    avg = moving_average(y, window_size).tolist()
-    residual = y - avg
-    # Calculate the variation in the distribution of the residual
-    std = np.std(residual)
-    return {'standard_deviation': round(std, 3),
-            'anomalies_dict': collections.OrderedDict([(index, y_i) for
-                                                       index, y_i, avg_i in zip(count(), y, avg)
-              if (y_i > avg_i + (sigma*std)) | (y_i < avg_i - (sigma*std))])}
-
-
-def explain_anomalies_rolling_std(y, window_size, sigma=1.0):
-    """ Helps in exploring the anamolies using rolling standard deviation
-    Args:
-    -----
-        y (pandas.Series): independent variable
-        window_size (int): rolling window size
-        sigma (int): value for standard deviation
-
-    Returns:
-    --------
-        a dict (dict of 'standard_deviation': int, 'anomalies_dict': (index: value))
-        containing information about the points indentified as anomalies
-    """
-    avg = moving_average(y, window_size)
-    avg_list = avg.tolist()
-    residual = y - avg
-    # Calculate the variation in the distribution of the residual
-    testing_std = pd.rolling_std(residual, window_size)
-    testing_std_as_df = pd.DataFrame(testing_std)
-    rolling_std = testing_std_as_df.replace(np.nan,
-                                  testing_std_as_df.ix[window_size - 1]).round(3).iloc[:,0].tolist()
-    std = np.std(residual)
-    return {'stationary standard_deviation': round(std, 3),
-            'anomalies_dict': collections.OrderedDict([(index, y_i)
-                                                       for index, y_i, avg_i, rs_i in zip(count(),
-                                                                                           y, avg_list, rolling_std)
-              if (y_i > avg_i + (sigma * rs_i)) | (y_i < avg_i - (sigma * rs_i))])}
-
-
-# This function is repsonsible for displaying how the function performs on the given dataset.
-def plot_results(x, y, window_size, sigma_value=1,
-                 text_xlabel="X Axis", text_ylabel="Y Axis", applying_rolling_std=False):
-    """ Helps in generating the plot and flagging the anamolies.
-        Supports both moving and stationary standard deviation. Use the 'applying_rolling_std' to switch
-        between the two.
-    Args:
-    -----
-        x (pandas.Series): dependent variable
-        y (pandas.Series): independent variable
-        window_size (int): rolling window size
-        sigma_value (int): value for standard deviation
-        text_xlabel (str): label for annotating the X Axis
-        text_ylabel (str): label for annotatin the Y Axis
-        applying_rolling_std (boolean): True/False for using rolling vs stationary standard deviation
-    """
-    plt.figure(figsize=(15, 8))
-    plt.plot(x, y, "k.")
-    y_av = moving_average(y, window_size)
-    plt.plot(x, y_av, color='green')
-    plt.xlim(0, 60)
-    plt.xlabel(text_xlabel)
-    plt.ylabel(text_ylabel)
-
-    # Query for the anomalies and plot the same
-    events = {}
-    if applying_rolling_std:
-        events = explain_anomalies_rolling_std(y, window_size=window_size, sigma=sigma_value)
-    else:
-        events = explain_anomalies(y, window_size=window_size, sigma=sigma_value)
-
-    x_anomaly = np.fromiter(events['anomalies_dict'], dtype=int, count=len(events['anomalies_dict']))
-    y_anomaly = np.fromiter(events['anomalies_dict'].values(), dtype=float,
-                                            count=len(events['anomalies_dict']))
-    print(x_anomaly, y_anomaly)
-    plt.plot(x_anomaly, y_anomaly, "r*", markersize=5)
-
-    # add grid and lines and enable the plot
-    plt.grid(True)
-    plt.show()
-
-
-# In[54]:
-
-
+from stolen_code import plot_results, explain_anomalies
 dfOutliers = dfmerged.sort_values(by='Period')
 x = dfOutliers[(dfOutliers.Industry == 'MATERIALS') & (dfOutliers.Product_Code == 'US20.3')].Period
 Y = dfOutliers[(dfOutliers.Industry == 'MATERIALS') & (dfOutliers.Product_Code == 'US20.3')].Price
 
 
-# In[67]:
+# In[27]:
 
 
 plot_results(x, y=Y, window_size=10, text_xlabel="Period", sigma_value=1.5,
@@ -299,9 +163,11 @@ plot_results(x, y=Y, window_size=10, text_xlabel="Period", sigma_value=1.5,
 events = explain_anomalies(Y, window_size=10, sigma=2)
 
 
-# Above red points show anomalies based on moving average std. Play around with different product codes, window sizes, sigmas, etc.
+# The above red points show anomalies based on moving average std. Can adjust for window sizes, sigmas, etc.
 
-# Show date (period) of highest and lowest Insight_Score per Industry.
+#  
+
+# *Show date (period) of highest and lowest Insight_Score per Industry.*
 
 # In[22]:
 
@@ -314,7 +180,7 @@ lowest = dfOutliers[idx_min][['Industry', 'Period', 'Insight_Score']].sort_value
 highest, lowest
 
 
-# Which industries are the most correlated?
+# *Which industries are the most correlated?*
 
 # In[23]:
 
